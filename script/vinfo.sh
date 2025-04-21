f7c31cbe645564a402dce12702944bfb0842df05
Yao Teng <95235602+tyoye@users.noreply.github.com>
2025-04-22 04:36:55 +0800

    Update vinfo.sh

--- script/vinfo.sh
+++ script/vinfo.sh
@@ -4,12 +4,20 @@
 ipv4=$(curl -s https://api.ipify.org)
 ipv6=$(ip -6 addr show scope global | grep inet6 | awk '{print $2}' | cut -d/ -f1 | head -n 1)
 
-# 获取地理位置数据（使用 ip-api.com）
-geo=$(curl -s https://ip-api.com/json/$ipv4)
+# 获取地理位置数据（使用 ipinfo.check.place）
+geo=$(curl -s "https://ipinfo.check.place/$ipv4")
 
-country=$(echo "$geo" | jq -r .country)
-region=$(echo "$geo" | jq -r .regionName)
-city=$(echo "$geo" | jq -r .city)
+# 解析 JSON 数据
+asn_number=$(echo "$geo" | jq -r .ASN.AutonomousSystemNumber)
+asn_org=$(echo "$geo" | jq -r .ASN.AutonomousSystemOrganization)
+city_name=$(echo "$geo" | jq -r .City.Name)
+postal_code=$(echo "$geo" | jq -r .City.PostalCode)
+latitude=$(echo "$geo" | jq -r .City.Latitude)
+longitude=$(echo "$geo" | jq -r .City.Longitude)
+timezone=$(echo "$geo" | jq -r .City.Location.TimeZone)
+country_name=$(echo "$geo" | jq -r .City.Country.Name)
+country_iso=$(echo "$geo" | jq -r .City.Country.IsoCode)
+region_name=$(echo "$geo" | jq -r .City.Subdivisions[0].Name)
 
 # SSH 端口，默认 22，可通过参数指定
 ssh_port=${1:-22}
@@ -18,7 +26,7 @@ ssh_port=${1:-22}
 read -p "请输入当前机器的提供商（如 Vultr、Linode）: " provider
 
 # name 格式：provider-region
-name="${provider}-${region}"
+name="${provider}-${region_name}"
 
 # 生成 name 的 SHA-1 哈希，取前 7 位
 hash=$(echo -n "$name" | sha1sum | cut -c1-7)
@@ -29,9 +37,17 @@ cat <<EOF
   "hash": "$hash",
   "name": "$name",
   "location": {
-    "country": "$country",
-    "region": "$region",
-    "city": "$city"
+    "country": "$country_name",
+    "region": "$region_name",
+    "city": "$city_name",
+    "postal_code": "$postal_code",
+    "latitude": $latitude,
+    "longitude": $longitude,
+    "timezone": "$timezone"
+  },
+  "ASN": {
+    "number": "$asn_number",
+    "organization": "$asn_org"
   },
   "provider": "$provider",
   "ip": {
